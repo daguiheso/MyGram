629fbc820fdc98938eee7bc170227379b8c088fe
Daniel H <frontend@reddesignsystems.com>
2016-05-18 13:24:01 -0500

    Add browserify, babelify and create gulp task for generate app.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 
 /*que tome nuestro file sass y lo compile a css*/
@@ -19,5 +22,15 @@ gulp.task('assets', function () {
 		.pipe(gulp.dest('public'));
 })
 
+/*procesar src/index.js y lo transforme utilizando browserify y bebelify*/
+gulp.task('scripts', function () {
+	browserify('./src/index.js') /*ruta de archivo que browserify procesara*/
+		.transform(babel)  /*que lo transforme utilizando babel - sin esta linea no utilizariamos ecma6*/
+		.bundle()  /*genera ese bundle*/
+		.pipe(source('index.js')) /* transformacion de lo que nos mando bundle() de browserify para que lo entienda gulp*/
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('public'))
+})
+
 /*default tarea*/
-gulp.task('default', ['styles', 'assets'])
+gulp.task('default', ['styles', 'assets', 'scripts'])
---
+++ src/index.js
@@ -0,0 +1,5 @@
+let numeros = [400, 200, 1, -23]
+
+let numerosMas1 = numeros.map(n => n+1)
+
+console.log(numerosMas1)
