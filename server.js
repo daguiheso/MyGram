1a7da987c32ef8a6b2bdba7048104bf0d6bfa2ea
Daniel H <frontend@reddesignsystems.com>
2016-05-18 10:52:21 -0500

    Add pub/jade, create view and configure motor pug/jade to express for views

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express();
 
+/*configurando motor de jade/pug para vistas*/
+app.set('view engine', 'pug');
+
 app.get('/', function (req,res) {
-	res.send('Hola mundo!');
+	res.render('index')
 })
 
 app.listen(3000, function (err) {
