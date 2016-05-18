2d0af6f13099041f49b275d5d8fc49a5de459775
Daniel H <frontend@reddesignsystems.com>
2016-05-18 16:19:23 -0500

    Automate build and add watchify

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 
 /*que tome nuestro file sass y lo compile a css*/
@@ -22,15 +23,34 @@ gulp.task('assets', function () {
 		.pipe(gulp.dest('public'));
 })
 
+
+function compile(watch) {
+	var bundle = watchify(browserify('./src/index.js')) /*ruta de archivo que browserify procesara*/
+
+	function rebundle() {
+		bundle
+			.transform(babel)  /*que lo transforme utilizando babel - sin esta linea no utilizariamos ecma6*/
+			.bundle()  /*genera ese bundle*/
+			.pipe(source('index.js')) /* transformacion de lo que nos mando bundle() de browserify para que lo entienda gulp*/
+			.pipe(rename('app.js'))
+			.pipe(gulp.dest('public'))
+	}
+
+	if (watch) {
+		bundle.on('update', function () {
+			console.log('--> Bundling...')
+			rebundle();
+		})
+	}
+	
+	rebundle();
+}
+
 /*procesar src/index.js y lo transforme utilizando browserify y bebelify*/
-gulp.task('scripts', function () {
-	browserify('./src/index.js') /*ruta de archivo que browserify procesara*/
-		.transform(babel)  /*que lo transforme utilizando babel - sin esta linea no utilizariamos ecma6*/
-		.bundle()  /*genera ese bundle*/
-		.pipe(source('index.js')) /* transformacion de lo que nos mando bundle() de browserify para que lo entienda gulp*/
-		.pipe(rename('app.js'))
-		.pipe(gulp.dest('public'))
-})
+gulp.task('build', function () { return compile(); });
+
+/*procesar y se mantiene escuchando a src/index.js y lo transforme utilizando browserify y bebelify*/
+gulp.task('watch', function () { return compile(true); });
 
 /*default tarea*/
-gulp.task('default', ['styles', 'assets', 'scripts'])
+gulp.task('default', ['styles', 'assets', 'build'])
