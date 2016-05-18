cc1c585bd0fca88b70149ae1a462a4c608bdf36d
Daniel H <frontend@reddesignsystems.com>
2016-05-18 13:46:18 -0500

    Add pagejs and implemented navigation

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ app.set('view engine', 'pug');
 
 /*Le decimos que public va a estar accesible*/
 app.use(express.static('public'))  /*app.use define un midleware, e indica a nuestro server que se sirva public de manera statica*/
-
+/* rutas */
 app.get('/', function (req,res) {
+	res.render('index')
+})
+app.get('/signup', function (req,res) {
+	res.render('index')
+})
+app.get('/signin', function (req,res) {
 	res.render('index')
 })
 
