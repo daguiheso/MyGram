cc376559d598d000913e613b019d46c3943c02b8
Daniel H <frontend@reddesignsystems.com>
2016-05-19 12:29:57 -0500

    Add picture-card module and render json objetc dummy

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -7,5 +7,27 @@ var title = require('title');
 page('/', function (ctx, next) {
 	title('MyGram');
 	var main = document.getElementById('main-container');
-	empty(main).appendChild(template);
+
+	var pictures = [
+		{
+			user: {
+				username: 'Daniel H',
+				avatar: 'https://scontent-mia1-1.xx.fbcdn.net/v/t1.0-9/12963889_10208901904603330_6870354961841214887_n.jpg?oh=f819a022e52be19cabaabd89d6cb39ce&oe=57E6B7F5'
+			},
+			url: 'http://materializecss.com/images/office.jpg',
+			likes: 4,
+			liked: true
+		},
+		{
+			user: {
+				username: 'David Pte',
+				avatar: 'https://scontent-mia1-1.xx.fbcdn.net/v/t1.0-9/13240476_10207468828932980_766208754586076978_n.jpg?oh=ae997f0f10403f65499674f5a2869757&oe=579DE806'
+			},
+			url: 'http://materializecss.com/images/office.jpg',
+			likes: 10,
+			liked: true
+		}
+	];
+
+	empty(main).appendChild(template(pictures));
 })
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,12 +1,17 @@
 var yo = require('yo-yo');
 var layout = require('../layout');
+var picture = require('../picture-card');
 
-var template = yo`<div class="conatiner timeline">
+module.exports = function (pictures) {
+
+	var el = yo`<div class="conatiner timeline">
 					<div class="row">
 						<div class="col s12 m10 offset-m1 l6 offset-l3">
-							content
+							${pictures.map(function (pic) {
+								return picture(pic);
+							})}
 						</div>
 					</div>
 				</div>`;
-
-module.exports = layout(template);
+	return layout(el);
+}
