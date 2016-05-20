a7ed643ba9a5fff2066270cb8907f8ee38e92925
Daniel H <frontend@reddesignsystems.com>
2016-05-20 17:31:20 -0500

    HTTP request with axios promises

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,14 +3,16 @@ var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
 var request = require('superagent');
-var headermidderware = require('../header')
+var headermidderware = require('../header');
+var axios = require('axios');
 
-page('/', headermidderware, loadPictures, function (ctx, next) {
+page('/', headermidderware, loadPicturesAxios, function (ctx, next) {
 	title('MyGram');
 	var main = document.getElementById('main-container');
 	empty(main).appendChild(template(ctx.pictures));
 })
 
+/*superagent*/
 function loadPictures(ctx, next) {
 	request
 		.get('/api/pictures')
@@ -20,4 +22,17 @@ function loadPictures(ctx, next) {
 			ctx.pictures = res.body  /*context  va cargando datos y los comparte a traves de los middleware*/
 			next();
 		})
+}
+
+/*axios*/
+function loadPicturesAxios(ctx, next) {
+	axios
+		.get('/api/pictures')
+		.then(function (res) {
+	    	ctx.pictures = res.data  /*context  va cargando datos y los comparte a traves de los middleware*/
+	    	next();
+	    })
+	  	.catch(function (err) {
+	    	console.log(err);
+	  	});	
 }
