e55a960b30a01b18fbd1ae2a7a146fdfc60532f4
Daniel H <frontend@reddesignsystems.com>
2016-05-20 11:12:45 -0500

    Add App texts to es and en-US

--- src/translate/es.js
+++ src/translate/es.js
@@ -1,3 +1,18 @@
 module.exports = {
-	likes: '{ likes, number} me gusta'   /*number es el tipo de dato*/
+	likes: '{ likes, number} me gusta',   /*number es el tipo de dato*/
+	logout: 'Salir',
+	english: 'Ingles',
+	Spanish: 'Español',
+	'signup.subheading': 'Registrate para ver fotos de tus amigos estudiantes',
+	'signup.facebook': 'Iniciar sesión con Facebook',
+    'signup.text': 'Iniciar sesión',
+    'email': 'Correo electrónico',
+    'username': 'Nombre de usuario',
+    'fullname': 'Nombre completo',
+    'password': 'Contraseña',
+    'signup.call-to-action': 'Regístrate',
+    'signup.have-account': '¿Tienes una cuenta?',
+    'signin': 'Entrar',
+    'signin.not-have-account': '¿No tienes una cuenta?',
+    'language': 'Idioma'
 }
